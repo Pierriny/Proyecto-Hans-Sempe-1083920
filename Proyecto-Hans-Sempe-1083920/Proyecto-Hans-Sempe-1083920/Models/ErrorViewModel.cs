a0a2c9186ef22c2ce6fe9785bbc42221ba9525e7
Pierriny <70544564+Pierriny@users.noreply.github.com>
2022-04-11 19:38:07 -0600

    creacion del objeto paciente

---
+++ Proyecto-Hans-Sempe-1083920/Proyecto-Hans-Sempe-1083920/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Proyecto_Hans_Sempe_1083920.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Proyecto-Hans-Sempe-1083920/Proyecto-Hans-Sempe-1083920/Models/Paciente.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_Hans_Sempe_1083920.Models
+{
+    public class Paciente
+    {
+        // constructor
+        public Paciente() { }
+
+        // obtiene y define el primer nombre del paciente
+        public String Nombre1 { get; set; }
+
+        // obtiene y define el segundo nombre del paciente
+        public String Nombre2 { get; set; }
+
+
+        // obtiene y define el Primer apellido del paciente
+        public String Apellido1 { get; set; }
+
+        // obtiene y define el Segundo apellido del paciente
+        public String Apellido2 { get; set; }
+
+        // obtiene y define el DPI del paciente
+        public int DPI { get; set; }
+
+        // obtiene y define la edad del paciente
+        public int Edad { get; set; }
+
+        // obtiene y define el numero telefonico del paciente
+        public int Telefono { get; set; }
+
+
+
+
+
+
+
+
+      
+        // obtiene y define una breve descripcion del ultimo diganostico o tratamiento actual
+        public String Descripcion { get; set; }
+
+
+
+
+    }
+}
